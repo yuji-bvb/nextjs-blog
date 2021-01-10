efb22e32fb9086bdacd394666ffb986743d1d0e1
yuji-bvb <yuji.bvb@gmail.com>
2021-01-10 23:10:21 +0900

    画像サイズ

--- pages/card.tsx
+++ pages/card.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function Blog({ card }) {
   const url = card.card_images[0];
-  const imgsrc = url.image_url;
+  const imgsrc = url.image_url_small;
   return (
     <>
       <Head>
