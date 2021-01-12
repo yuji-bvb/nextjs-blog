cdd123bf5f0c20a3b72754a7744bf87849eca70e
yuji-bvb <yuji.bvb@gmail.com>
2021-01-12 10:24:31 +0900

    💬 問合せ追加

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,6 +4,11 @@ export function Footer(): JSX.Element {
   return (
     <footer className={styles.container}>
       <small>@2020 yuji yanase</small>
+      <div>
+        <a href="mailto:yuji.bvb@gmail.com?">
+          <small>お問い合わせ</small>
+        </a>
+      </div>
     </footer>
   );
 }
