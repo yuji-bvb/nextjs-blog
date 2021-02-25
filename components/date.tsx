950b0008df5b05432397346afde03319dc6615cd
yuji-bvb <yuji.bvb@gmail.com>
2021-02-25 22:16:29 +0900

    ♻️ ディレクトリ、名前変更

--- components/date.tsx
+++
@@ -1,7 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-const Date = ({ dateString }: { dateString: string }) => {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
-}
-export default Date
--- lib/card.ts
+++
@@ -1,8 +0,0 @@
-import fetch from "node-fetch";
-const apiUrl = "https://db.ygoprodeck.com/api/v7/randomcard.php";
-
-export async function getRandomCardData() {
-  const res = await fetch(new URL(apiUrl));
-  const card = await res.json();
-  return card;
-}
